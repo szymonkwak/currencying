cd6e79de702af8b4b4b98979ad7520b3326222b7
Szymon Kwak <szymon.kwak@gmail.com>
2022-04-17 11:24:24 +0200

    del h & l

--- client/src/components/header/CurrencyingLogo.tsx
+++
@@ -1,7 +0,0 @@
-import { Box } from '@mui/material';
-
-const CurrencyingLogo = () => {
-  return <Box component="img" height="50px" sx={{ mr: 3 }} alt="logo" src="/currencying.svg" />;
-};
-
-export default CurrencyingLogo;
