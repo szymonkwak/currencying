1945734eaf09f90e9faabdcc17d6afd22d378ddf
Szymon Kwak <szymon.kwak@gmail.com>
2022-04-17 11:25:45 +0200

    refactor H & L

---
+++ client/src/components/Header/headerButtons.ts
@@ -0,0 +1,13 @@
+import routes from '../../routes';
+
+export const headerButtons = [
+  { label: 'Home', href: routes.root },
+  { label: 'Wallet', href: routes.pulpit },
+  { label: 'Exchange', href: routes.exchange },
+];
+
+export const loginButtons = {
+  SignUp: { label: 'Sing up', href: routes.signUp },
+  SignIn: { label: 'Sign in', href: routes.signIn },
+  Logout: { label: 'Logout', href: routes.logout },
+};
